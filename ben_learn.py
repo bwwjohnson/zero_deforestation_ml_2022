e0f1551932d42346dfef552fdccdbb8ef0772d11
DBHG <58551070+DBHG@users.noreply.github.com>
2022-11-19 20:29:44 +0000

    Update ben_learn.py

--- ben_learn.py
+++ ben_learn.py
@@ -199,5 +199,29 @@ traintime()
     # # ==================================================================================
 
     # # DAN's TUTORIAL
+# for the loss plots it basically requires you to save the accuary and loss at each epoch. the way I thought of doing it was  to try and get it to output
+# them to a np.array and then just plot them against an np.range(#epochs). 
 
+#I can see in line 187-190 you are making the right thing, just wondering if you also get accuary out of it as well as that would be a usefu thing for the report
+'''
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()'''
